e62fd1ac475a0603b5f45642a6aef17c7bf3628f
Nimo Liu <nimo.r.liu@gmail.com>
2026-07-02 20:31:11 -0400

    Revert "x"
    This reverts commit acfdde43c4954ebc9601aa9254c2f527f2d87964.

--- home-inspection/src/main/java/ca/inspection/home/inspection/entity/InspectionRecommendationField.java
+++ home-inspection/src/main/java/ca/inspection/home/inspection/entity/InspectionRecommendationField.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
-import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -26,11 +25,11 @@ public class InspectionRecommendationField {
     @JoinColumn(name = "inspection_field_id")
     private InspectionField inspectionField;
 
-    private List<String> direction; //direction in the house
-    private List<String> floorLevel; //which floor is it on?
-    private List<String> room; //which room is it in?
-    private List<String> task; //what recommendation are we making?
-    private List<String> time; //when do we have to fix it by?
+    private String direction; //direction in the house
+    private String floorLevel; //which floor is it on?
+    private String room; //which room is it in?
+    private String task; //what recommendation are we making?
+    private String time; //when do we have to fix it by?
     private String lower_cost; //whats the lower bound
     private String upper_cost; //whats the upper bound
 
