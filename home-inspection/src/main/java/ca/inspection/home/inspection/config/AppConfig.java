1635086ebbdc050d0ce19e7b1c27c379d34c1931
Nimo Liu <nimo.r.liu@gmail.com>
2026-09-12 15:47:48 -0400

    report generation -> docker container does not use dev server anymore. config timeout + ram usage

--- home-inspection/src/main/java/ca/inspection/home/inspection/config/AppConfig.java
+++ home-inspection/src/main/java/ca/inspection/home/inspection/config/AppConfig.java
@@ -1,5 +1,6 @@
 package ca.inspection.home.inspection.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -8,11 +9,15 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
+    // Read timeout for report generation
+    @Value("${pdf.service.read-timeout-ms:600000}")
+    private int pdfReadTimeoutMs;
+
     @Bean
     public RestTemplate restTemplate(){
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(10000);
-        factory.setReadTimeout(60000);
+        factory.setReadTimeout(pdfReadTimeoutMs);
         return new RestTemplate(factory);
     }
 }
